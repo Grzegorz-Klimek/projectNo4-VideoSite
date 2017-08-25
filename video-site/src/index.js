06188db21432672d095077edc7b6b89526574f9e
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-08-25 10:40:02 +0200

    create search-bar content

--- video-site/src/index.js
+++ video-site/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from "./components/search-bar";
 
 const API_KEY = 'AIzaSyDnLq67jGMjUp1e84MYEqjVdHSQ1EELCmA';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return <SearchBar />;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container') );
