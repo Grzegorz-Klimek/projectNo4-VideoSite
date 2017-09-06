2d0250bc0d79b64a30baacc8021d23358a92c50a
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-09-06 12:52:10 +0200

    take data from youtube API

--- video-site/src/components/video-list-item.js
+++ video-site/src/components/video-list-item.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url
+
     return (
         <li className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
-                    <img alt="" className="media-object"/>
+                    <img src={imageUrl} alt="YTThumbnail" className="media-object"/>
                 </div>
                 <div className="media-body">
                     <div className="media-heading">
-
+                        {video.snippet.title}
                     </div>
                 </div>
             </div>
