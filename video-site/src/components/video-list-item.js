37a154a0039a121655c2d450e211dd9d07ac4c64
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-09-06 12:47:19 +0200

    make video-list-item structure

--- video-site/src/components/video-list-item.js
+++ video-site/src/components/video-list-item.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
     return (
-        <li>Video</li>
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img alt="" className="media-object"/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+
+                    </div>
+                </div>
+            </div>
+        </li>
     )
 }
 
