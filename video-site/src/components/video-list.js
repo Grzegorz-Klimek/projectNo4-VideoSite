ad1ec059f5d035efb23ee116754690c4f69ec351
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-09-06 12:32:25 +0200

    map list of videos

--- video-site/src/components/video-list.js
+++ video-site/src/components/video-list.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import VideoListItem from './video-list-item'
 
-const VideoList = () => {
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video}/>
+    })
+
     return (
-        <ul>
-
+        <ul className="col-md-4 list-group">
+            {videoItems}
         </ul>
     )
 }
+
+export default VideoList
--- video-site/src/index.js
+++ video-site/src/index.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import SearchBar from "./components/search-bar"
-import YTSearch from 'youtube-api-search'
-
 import { Grid } from 'react-bootstrap'
+
+import SearchBar from "./components/search-bar"
+import VideoList from "./components/video-list"
+
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyDnLq67jGMjUp1e84MYEqjVdHSQ1EELCmA'
 
 class App extends Component {
     constructor(props) {
         super(props)
+
         this.state = { videos: [] }
+
         YTSearch({key: API_KEY, term: 'Iron Maiden'}, (videos) => {
             this.setState({ videos })
         })
@@ -21,6 +25,7 @@ class App extends Component {
             <Grid fluid>
                 <div>
                     <SearchBar/>
+                    <VideoList videos={this.state.videos}/>
                 </div>
             </Grid>
         )
