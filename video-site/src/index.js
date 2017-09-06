84bf83ac035e044419c4c3f2d0e9bc4a79c23a20
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-09-06 11:12:19 +0200

    create state with videos

--- video-site/src/index.js
+++ video-site/src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from "./components/search-bar";
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from "./components/search-bar"
+import YTSearch from 'youtube-api-search'
 
-const API_KEY = 'AIzaSyDnLq67jGMjUp1e84MYEqjVdHSQ1EELCmA';
+const API_KEY = 'AIzaSyDnLq67jGMjUp1e84MYEqjVdHSQ1EELCmA'
 
-const App = () => {
-    return <SearchBar />;
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { videos: [] }
+        YTSearch({key: API_KEY, term: 'Iron Maiden'}, (videos) => {
+            this.setState({ videos })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container') );
+ReactDOM.render(<App/>, document.querySelector('.container'))
